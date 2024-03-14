e1812465ee0e6a86c46bb1e4283e9455fc6d4da5
Vinicius Gabriel <madara5004br@gmail.com>
2024-03-14 15:29:38 -0300

    arrumado o bug de variável indefinida

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -22,28 +22,31 @@ function openModal(countryName){
 
     let country = getCountryObject(countryName);
 
-    let htmlContent = 
-    `<div class="images-container">
-        <img src="${country.flags.svg}" alt="flag">
-        <img src="${country.coatOfArms.svg}" alt="coat of arms">
-    </div>
-    <div class="view-on-map"><a href="${country.maps.googleMaps}" target="_blank" class="button bg-gradient">View on Map</a></div>
-    <ul class="country-about">
-        <li><strong>Official Name: </strong>${country.name.official}</li>
-        <li><strong>Common Name: </strong>${country.name.common}</li>
-        <li><strong>Capital: </strong>${country.capital[0]}</li>
-        <li><strong>Region: </strong>${country.region}</li>
-        <li><strong>Subregion: </strong>${country.subregion}</li>
-        <li><strong>Population: </strong>${country.population}</li>
-        <li><strong>LatLng: </strong>[${country.latlng}]</li>
-        <li><strong>Time Zones: </strong>[${country.timezones}]</li>
-        <li><strong>Borders: </strong>${country.borders ?? 'none'}</li>
-        <li><strong>Landlocked: </strong>${country.landlocked ? 'Yes' : 'No'}</li>
-        <li><strong>Independet? </strong>${country.independent ? 'Yes' : 'No'}</li>
-        <li><strong>United Nations member?  </strong>${country.unMember ? 'Yes' : 'No'}</li>
-    </ul>`;
+    if(country){
+        let htmlContent = 
+        `<div class="images-container">
+            <img src="${country.flags.svg}" alt="flag">
+            <img src="${country.coatOfArms.svg}" alt="coat of arms">
+        </div>
+        <div class="view-on-map"><a href="${country.maps.googleMaps}" target="_blank" class="button bg-gradient">View on Map</a></div>
+        <ul class="country-about">
+            <li><strong>Official Name: </strong>${country.name.official}</li>
+            <li><strong>Common Name: </strong>${country.name.common}</li>
+            <li><strong>Capital: </strong>${country.capital ? country.capital.join(', ') : 'none'}</li>
+            <li><strong>Languages: </strong>${Object.values(country.languages).join(', ')}</li>
+            <li><strong>Region: </strong>${country.region}</li>
+            <li><strong>Subregion: </strong>${country.subregion ?? 'none'}</li>
+            <li><strong>Population: </strong>${country.population}</li>
+            <li><strong>Landlocked: </strong>${country.landlocked ? 'Yes' : 'No'}</li>
+            <li><strong>Independet? </strong>${country.independent ? 'Yes' : 'No'}</li>
+            <li><strong>United Nations member?  </strong>${country.unMember ? 'Yes' : 'No'}</li>
+            <li><strong>LatLng: </strong>[${country.latlng.map(value => value.toFixed(2)).join(', ')}]</li>  
+            <li><strong>Borders: </strong>${country.borders ? country.borders.join(', ') : 'none'}</li>
+            <li><strong>Time Zones: </strong>[${country.timezones.join(', ')}]</li>
+        </ul>`;
 
-    countryInfo.innerHTML = htmlContent;
+        countryInfo.innerHTML = htmlContent;
+    }
 
     modalContainer.style.opacity = 1;
     modalContainer.style.visibility = 'visible';
